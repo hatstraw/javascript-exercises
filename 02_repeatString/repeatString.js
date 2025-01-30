04f856198103bff0c8b94740ed95e63913663ced
hatstraw <ca21032saravanan@gmail.com>
2025-01-30 08:03:50 +0800

    Finished 01-03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string='', num=1) {
+    let repeatedString = '';
+    if (num < 0) return 'ERROR';
 
+    while(num){
+        repeatedString += string;
+        num--;
+    }
+    return repeatedString;
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
